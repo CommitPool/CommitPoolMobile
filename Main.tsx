f9c37b5a263c7cd7731e77125517e16decedcbb6
bitbeckers <code@bitbeckers.com>
2020-12-16 14:29:44 +0100

    fixed welcome > login forwarding

--- Main.tsx
+++ Main.tsx
@@ -1,100 +1,116 @@
-
 import React, { Component } from "react";
 import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
-import Login from './Login'
-import Commit from './Commit'
-import Track from './Track'
-import MakeCommitment from './MakeCommitment'
-import Complete from './Complete'
-import Wallet from './Wallet'
-import Welcome from './Welcome'
+import Login from "./Login";
+import Commit from "./Commit";
+import Track from "./Track";
+import MakeCommitment from "./MakeCommitment";
+import Complete from "./Complete";
+import Wallet from "./Wallet";
+import Welcome from "./Welcome";
 
-import { Dimensions } from 'react-native';
-import {LinearGradient} from 'expo-linear-gradient'
+import { Dimensions } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-export default class Main extends Component <{account: any, stravaOAuth: any, code: string}, {step: Number, account: any}> {
+export default class Main extends Component<
+  { account: any; stravaOAuth: any; code: string },
+  { step: Number; account: any }
+> {
   constructor(props) {
     super(props);
     this.state = {
       step: 1,
-      account: undefined
+      account: undefined,
     };
   }
 
-  componentWillReceiveProps (newProps) {
-    if(newProps.code !== this.props.code) {
-      this.setState({step: 4})
+  componentWillReceiveProps(newProps) {
+    if (newProps.code !== this.props.code) {
+      this.setState({ step: 4 });
     }
   }
 
   onClick = (step: Number) => {
-    this.setState({step: step})
-  }
+    this.setState({ step: step });
+  };
 
   renderSwitch = () => {
-    switch(this.state.step) {
-        case 1: 
-            return <Welcome next={this.onClick}></Welcome>
-        case 2: 
-            return <Login next={this.onClick} stravaOAuth={this.props.stravaOAuth} code={this.props.code}></Login>
-        case 4:
-            return (
- 		<LinearGradient
-          colors={['#D45353', '#D45353', 'white' ]}
-          style={styles.linearGradient}
-        >
-                <Wallet next={this.onClick} account={this.props.account}></Wallet>
-	</LinearGradient>
-            )
-        case 5:
-            return (
-		<LinearGradient
-          		colors={['#D45353', '#D45353', 'white' ]}
-          		style={styles.linearGradient}
-        		>
-                <Commit next={this.onClick}></Commit>
-		</LinearGradient>
-            )
-        case 6:
-            return (
-			<LinearGradient
-          		colors={['#D45353', '#D45353', 'white' ]}
-          		style={styles.linearGradient}
-        		>
-                <MakeCommitment next={this.onClick} account={this.props.account} code={this.props.code}></MakeCommitment>
-</LinearGradient>
-            )
-        case 7:
-            return (
-                <Track next={this.onClick} account={this.props.account} code={this.props.code}></Track>
-            )
-        case 8:
-            return (
-                <Complete next={this.onClick} account={this.props.account}></Complete>
-            )
-      }
-  }
+    switch (this.state.step) {
+      case 1:
+        return (
+          <Welcome
+            next={this.onClick}
+            stravaOAuth={this.props.stravaOAuth}
+            code={this.props.code}
+          ></Welcome>
+        );
+      case 2:
+        return (
+          <Login
+            next={this.onClick}
+            stravaOAuth={this.props.stravaOAuth}
+            code={this.props.code}
+          ></Login>
+        );
+      case 4:
+        return (
+          <LinearGradient
+            colors={["#D45353", "#D45353", "white"]}
+            style={styles.linearGradient}
+          >
+            <Wallet next={this.onClick} account={this.props.account}></Wallet>
+          </LinearGradient>
+        );
+      case 5:
+        return (
+          <LinearGradient
+            colors={["#D45353", "#D45353", "white"]}
+            style={styles.linearGradient}
+          >
+            <Commit next={this.onClick}></Commit>
+          </LinearGradient>
+        );
+      case 6:
+        return (
+          <LinearGradient
+            colors={["#D45353", "#D45353", "white"]}
+            style={styles.linearGradient}
+          >
+            <MakeCommitment
+              next={this.onClick}
+              account={this.props.account}
+              code={this.props.code}
+            ></MakeCommitment>
+          </LinearGradient>
+        );
+      case 7:
+        return (
+          <Track
+            next={this.onClick}
+            account={this.props.account}
+            code={this.props.code}
+          ></Track>
+        );
+      case 8:
+        return (
+          <Complete next={this.onClick} account={this.props.account}></Complete>
+        );
+    }
+  };
 
   render() {
-    return (
-        <View style={{flex: 1}}>
-            {this.renderSwitch()}
-        </View>
-    );
+    return <View style={{ flex: 1 }}>{this.renderSwitch()}</View>;
   }
 }
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-
-linearGradient: {
+  linearGradient: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     width,
     height,
-    borderRadius: 5
-  }
-
+    borderRadius: 5,
+  },
 });
--- Welcome.tsx
+++ Welcome.tsx
@@ -1,51 +1,55 @@
-import * as React from 'react';
+import * as React from "react";
 import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
-import {LinearGradient} from 'expo-linear-gradient'
-import { Dimensions } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import { Dimensions } from "react-native";
 
-export default  class Welcome extends React.Component <{next: any}, {}>{
-
+export default class Welcome extends React.Component<
+  { stravaOAuth: any; next: any; code: string },
+  {}
+> {
   render() {
     return (
       <LinearGradient
-      colors={['#D45353', '#D45353', 'white' ]}
-      style={styles.linearGradient}
+        colors={["#D45353", "#D45353", "white"]}
+        style={styles.linearGradient}
       >
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'space-around'}}>
-          <View style={{alignItems: 'center'}}>
-              <Image
-                  style={{width: 200, height: 200}}
-                  source={require('./assets/commit.png')}
-              />
-              <Text style={{ color: 'white', fontSize: 50}}>
-                  This is the welcome
-              </Text>
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <View style={{ alignItems: "center" }}>
+            <Image
+              style={{ width: 200, height: 200 }}
+              source={require("./assets/commit.png")}
+            />
+            <Text style={{ color: "white", fontSize: 50 }}>
+              This is the welcome
+            </Text>
           </View>
-          <TouchableOpacity onPress={this.props.next(2)}>
-          <Text style={{ color: 'white', fontSize: 50}}>
-                  This is the welcome
-              </Text>
-              <Image
-                  style={{width: 300, height: 50}}
-                  source={require('./assets/strava.svg')}
-              />
+          <TouchableOpacity onPress={() => this.props.next(2)}>
+            <Image
+              style={{ width: 300, height: 50 }}
+              source={require("./assets/strava.svg")}
+            />
           </TouchableOpacity>
-      </View>
-    </LinearGradient>)
+        </View>
+      </LinearGradient>
+    );
   }
 }
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-
   linearGradient: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      width,
-      height,
-      borderRadius: 5
-    }
-  
-  });
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width,
+    height,
+    borderRadius: 5,
+  },
+});
